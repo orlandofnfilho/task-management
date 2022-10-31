96edc973c68eecabfce44a80ec4ad86d6ff8625f
Orlando Ferreira <orlandofnfilho@gmail.com>
2022-10-31 09:08:00 -0300

    feat: add auth, update and delete

--- app/models.py
+++ app/models.py
@@ -1,9 +1,6 @@
-from email.policy import default
-from operator import truediv
-from tkinter import CASCADE
-from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import User
+
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'app.apps.AppConfig'
 ]
 
 MIDDLEWARE = [
@@ -112,11 +112,13 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOGIN_URL = 'login'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

7e0035456d94b6a36d20419c591dca258155c88a
Orlando Ferreira <orlandofnfilho@gmail.com>
2022-11-12 23:01:20 -0300

    feat: changed task list & add css

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from ast import Delete
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
@@ -36,6 +36,12 @@ class RegisterPage(FormView):
             login(self.request, user)
         return super(RegisterPage, self).form_valid(form)
 
+# Se o usuário estiver autenticado redireciona para página de tasks
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
 
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
@@ -46,6 +52,14 @@ class TaskList(LoginRequiredMixin, ListView):
         context['task_list'] = context['task_list'].filter(
             user=self.request.user)
         context['count'] = context['task_list'].filter(complete=False).count()
+        
+        #filtrar tasks pelo nome
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['task_list'] = context['task_list'].filter(
+                title__contains=search_input)
+
+        context['search_input'] = search_input
         return context
 
 
